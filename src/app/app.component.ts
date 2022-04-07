ea44f9b23c94bc739e68a841723a0664a639923c
ipula <ipularanasinghe007@gmail.com>
2022-04-07 10:14:11 +0530

    6 files modified, 2 deleted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,53 @@
-import { Component, VERSION } from '@angular/core';
+import {
+  Component,
+  Input,
+  OnInit,
+  OnChanges,
+  ViewChild,
+  SimpleChanges,
+} from '@angular/core';
+import { GeoJSONSourceComponent } from 'ngx-mapbox-gl';
 
 @Component({
   selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  template: `
+  <mgl-map
+    [style]="'mapbox://styles/mapbox/streets-v9'"
+    [zoom]="[9]"
+    [center]="[-74.50, 40]"
+  ></mgl-map>
+  `,
+  styles: [
+    `
+    mgl-map {
+      height: 100vh;
+      width: 100vw;
+    }
+  `,
+  ],
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent {
+  earthquakes: GeoJSON.FeatureCollection;
+  selectedCluster: { geometry: GeoJSON.Point; properties: any };
+
+  async ngOnInit() {
+    const earthquakes: GeoJSON.FeatureCollection = <any>(
+      await import('./earthquakes.geo.json')
+    );
+    setInterval(() => {
+      if (earthquakes.features.length) {
+        earthquakes.features.pop();
+      }
+      this.earthquakes = { ...earthquakes };
+    }, 500);
+  }
+
+  selectCluster(event: MouseEvent, feature: any) {
+    event.stopPropagation(); // This is needed, otherwise the popup will close immediately
+    // Change the ref, to trigger mgl-popup onChanges (when the user click on the same cluster)
+    this.selectedCluster = {
+      geometry: feature.geometry,
+      properties: feature.properties,
+    };
+  }
 }
